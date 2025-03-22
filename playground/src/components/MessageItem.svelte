303fe17b3106007f0aa2f4d1fce14ee9e11caab2
Eran Knafo <eranknafo2001@gmail.com>
2025-03-22 17:24:54 +0200

    wip

--- playground/src/components/MessageItem.svelte
+++ playground/src/components/MessageItem.svelte
@@ -5,10 +5,26 @@
   import sanitizeHtml from "sanitize-html";
   import { Message } from "../lib/types";
 
+  import dayjs from "dayjs";
+  import relativeTime from "dayjs/plugin/relativeTime";
+
+  dayjs.extend(relativeTime);
+
   interface Props {
     message: Message;
   }
   let { message }: Props = $props();
+
+  let currentTime = $state(new Date());
+
+  $effect(() => {
+    const interval = setInterval(() => {
+      currentTime = new Date();
+    }, 60000);
+    return () => clearInterval(interval);
+  });
+
+  let time = $derived(dayjs(message.time).from(currentTime));
 </script>
 
 <div class="chat {message.type === 'user' ? 'chat-end' : 'chat-start'}">
@@ -27,5 +43,8 @@
     {:else}
       {JSON.stringify(message.value)}
     {/if}
+  </div>
+  <div class="chat-footer text-xs opacity-50">
+    {time}
   </div>
 </div>
