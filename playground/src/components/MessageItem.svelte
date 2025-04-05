1c0df0b677da330247a266c755f95a6ed798d44c
Itay Sin Malia <itays.malia@gmail.com>
2025-04-05 22:02:05 +0300

    chore: some fixes

--- playground/src/components/MessageItem.svelte
+++ playground/src/components/MessageItem.svelte
@@ -4,6 +4,8 @@
   import sanitizeHtml from "sanitize-html";
   import "svelte-highlight/styles/github-dark.css";
   import { Message } from "../lib/types";
+  import Convert from "ansi-to-html";
+  let convert = new Convert();
 
   import dayjs from "dayjs";
   import relativeTime from "dayjs/plugin/relativeTime";
@@ -33,12 +35,14 @@
   <div class="chat-header">
     {message.type}
   </div>
-  <div class="chat-bubble bg-slate-950 before:bg-slate-950">
+  <div
+    class="chat-bubble bg-slate-950 before:bg-slate-950 max-h-[80vh] overflow-auto"
+  >
     {#if message.type === "user"}
       <Highlight language={shell} code={message.value} class="shadow-xl" />
     {:else if "success" in message.value}
       <div class="response-content">
-        {@html sanitizeHtml(message.value.success.String.val)}
+        {@html convert.toHtml(sanitizeHtml(message.value.success.String.val))}
       </div>
     {:else}
       {JSON.stringify(message.value)}
@@ -49,52 +53,53 @@
   </div>
 </div>
 
-<style>
-  :global {
-    @import "tailwindcss";
+<style lang="postcss" global>
+  @reference "tailwindcss";
+  :global(.response-content div) {
+    @apply text-gray-200 font-mono whitespace-pre-wrap;
+  }
 
-    .response-content div {
-      background-color: transparent !important;
-      @apply text-gray-200! font-mono! whitespace-pre-wrap;
-    }
+  :global(.response-content ol) {
+    @apply max-w-md space-y-1 list-decimal list-inside p-1;
+    list-style: none;
+  }
 
-    .response-content ol {
-      @apply max-w-md space-y-1 list-decimal list-inside p-1;
-      list-style: none;
-    }
-    .response-content ol li {
-      margin: 0 !important;
-    }
-    .response-content ol li:not(:first-child) {
-      counter-increment: list-counter 1;
-    }
-    .response-content ol li::before {
-      content: counter(list-counter) " | ";
-      @apply font-bold text-gray-600;
-    }
+  :global(.response-content ol li) {
+    margin: 0;
+  }
+  :global(.response-content ol li:not(:first-child)) {
+    counter-increment: list-counter 1;
+  }
+  :global(.response-content ol li)::before {
+    content: counter(list-counter) " | ";
+    @apply font-bold text-gray-600;
+  }
 
-    .response-content table {
-      @apply table-auto text-sm text-left text-gray-400 shadow-xl;
-    }
+  :global(.response-content table) {
+    @apply table-auto text-sm text-left text-gray-400 shadow-xl;
+  }
 
-    .response-content thead {
-      @apply text-xs bg-gray-600 text-gray-400;
-    }
-    .response-content thead th {
-      @apply px-6 py-3;
-    }
-    .response-content thead th:nth-child(even) {
-      @apply bg-gray-700;
-    }
+  :global(.response-content thead) {
+    @apply text-xs bg-gray-600 text-gray-400;
+  }
 
-    .response-content tbody tr {
-      @apply border-b bg-gray-800 border-gray-700;
-    }
-    .response-content tbody td {
-      @apply px-6 py-4;
-    }
-    .response-content tbody td:nth-child(even) {
-      @apply bg-gray-900;
-    }
+  :global(.response-content thead th) {
+    @apply px-6 py-3;
+  }
+
+  :global(.response-content thead th:nth-child(even)) {
+    @apply bg-gray-700;
+  }
+
+  :global(.response-content tbody tr) {
+    @apply border-b bg-gray-800 border-gray-700;
+  }
+
+  :global(.response-content tbody td) {
+    @apply px-6 py-4;
+  }
+
+  :global(.response-content tbody td:nth-child(even)) {
+    @apply bg-gray-900;
   }
 </style>
