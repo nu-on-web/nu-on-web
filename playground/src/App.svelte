9358471879b1938533598cd488f4807e835c27f3
eran <eranknafo2001@gmail.com>
2025-04-24 23:32:14 +0300

    feat: presistent files (#70)

--- playground/src/App.svelte
+++ playground/src/App.svelte
@@ -1,12 +1,26 @@
 <script lang="ts">
   import "./app.css";
   import Playground from "./Playground.svelte";
-  import { fs } from "@zenfs/core";
+  import { configureSingle } from "@zenfs/core";
+  import { mkdir, writeFile, exists } from "@zenfs/core/promises";
+  import { WebStorage } from "@zenfs/dom";
 
-  $effect(() => {
-    fs.mkdirSync("/files");
-    fs.writeFileSync("/files/example.json", "[{v:1},{v:2},{v:3}]");
-  });
+  let fsSetupPromise = (async () => {
+    await configureSingle({ backend: WebStorage });
+    await mkdir("/files", { recursive: true });
+    if (!(await exists("/files/example.json")))
+      await writeFile("/files/example.json", "[{v:1},{v:2},{v:3}]");
+  })().catch((error) =>
+    console.error("Error in setting up the filesystem:", error),
+  );
 </script>
 
-<Playground />
+{#await fsSetupPromise}
+  <div class="flex items-center justify-center h-screen">
+    <span class="loading loading-spinner loading-lg"></span>
+  </div>
+{:then}
+  <Playground />
+{/await}
+
+<!-- <Playground /> -->
