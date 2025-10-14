14871c95e35ce6f3305dcf0a072edcdb7b7a4588
Eran Knafo <eranknafo2001@gmail.com>
2025-10-14 06:06:00 +0300

    Remove unused Zod dependency and validation

--- playground/src/lib/types.ts
+++ playground/src/lib/types.ts
@@ -4,21 +4,3 @@ export type Message = (
   | { type: "user"; value: string }
   | { type: "nushell"; value: RunCodeResult }
 ) & { time: Date };
-
-// export const Message = z
-//   .discriminatedUnion("type", [
-//     z.object({
-//       type: z.literal("user"),
-//       value: z.string(),
-//     }),
-//     z.object({
-//       type: z.literal("nushell"),
-//       value: z.custom<RunCodeResult>((t) => t),
-//     }),
-//   ])
-//   .and(
-//     z.object({
-//       time: z.date(),
-//     }),
-//   );
-// export type Message = z.infer<typeof Message>;
