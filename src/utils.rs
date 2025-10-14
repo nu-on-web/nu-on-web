45f20952136057353290bddc1db3185897f8257b
Eran Knafo <eranknafo2001@gmail.com>
2025-10-14 21:34:14 +0300

    Add warn() console binding to utils

--- src/utils.rs
+++ src/utils.rs
@@ -4,6 +4,7 @@ use wasm_bindgen::prelude::*;
 extern "C" {
     #[wasm_bindgen(js_namespace = console)]
     pub fn log(s: &str);
+    #[wasm_bindgen(js_namespace = console)]
     pub fn warn(s: &str);
 }
 
