56a4c8d36d6875351e243340fd2e6ee917d4cc0d
Eran Knafo <eranknafo2001@gmail.com>
2025-10-14 06:45:05 +0300

    Use Stats bindings from wasm-bindgen directly

--- src/zenfs.rs
+++ src/zenfs.rs
@@ -1,12 +1,6 @@
 use js_sys::{Error, Object, Reflect};
 use wasm_bindgen::prelude::*;
 
-#[derive(Debug)]
-pub struct Stats {
-    pub is_directory: bool,
-    pub size: u32,
-}
-
 #[wasm_bindgen(module = "@zenfs/core")]
 extern "C" {
     #[wasm_bindgen(js_namespace = fs, js_name = readFileSync, catch)]
@@ -14,35 +8,23 @@ extern "C" {
     #[wasm_bindgen(js_namespace = fs, js_name = readdirSync, catch)]
     pub fn readdir(path: &str) -> Result<Vec<String>, Error>;
     #[wasm_bindgen(js_namespace = fs, js_name = statSync, catch)]
-    fn stat_js(path: &str) -> Result<JsValue, Error>;
+    pub fn stat(path: &str) -> Result<Stats, Error>;
     #[wasm_bindgen(js_namespace = fs, js_name = unlinkSync, catch)]
     pub fn unlink(path: &str) -> Result<(), Error>;
 }
 
-pub fn stat(path: &str) -> Result<Stats, Error> {
-    let value = stat_js(path)?;
-    if !value.is_object() {
-        return Err(Error::new("Failed to get stats"));
+#[wasm_bindgen]
+extern "C" {
+    pub type Stats;
+
+    #[wasm_bindgen(structural, method, js_name = isDirectory)]
+    pub fn is_directory(this: &Stats) -> bool;
+}
+
+impl Stats {
+    pub fn size(&self) -> u32 {
+        let v = Reflect::get(self.as_ref(), &JsValue::from_str("size"))
+            .expect("Failed to get 'size' property from Stats object");
+        v.as_f64().expect("Stats 'size' property should be a numeric value") as u32
     }
-
-    let is_directory = Reflect::get(&value, &JsValue::from_str("isDirectory"))
-        .map_err(|_| Error::new("Failed to get isDirectory"))?;
-    if !is_directory.is_function() {
-        return Err(Error::new("Failed to get isDirectory"));
-    }
-    let is_directory = is_directory
-        .dyn_into::<js_sys::Function>()
-        .map_err(|_| Error::new("Failed to get isDirectory"))?;
-    let is_directory = is_directory
-        .call0(&value)
-        .map_err(|_| Error::new("Failed to call isDirectory"))?
-        .as_bool()
-        .ok_or_else(|| Error::new("Failed to get isDirectory"))?;
-
-    let size = Reflect::get(&value, &JsValue::from_str("size"))
-        .map_err(|_| Error::new("Failed to get size"))?
-        .as_f64()
-        .ok_or_else(|| Error::new("Failed to get size"))? as u32;
-
-    Ok(Stats { is_directory, size })
 }
