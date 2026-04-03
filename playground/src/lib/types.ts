5b6d3326a97cb82a0cc1dc7c8fa0f283c7a15d16
smalia2001 <smalia2001@users.noreply.github.com>
2026-04-03 15:15:05 +0000

    Commit from GitHub Actions (Deploy To Github Pages)

--- playground/src/lib/types.ts
+++
@@ -1,6 +0,0 @@
-import type { RunCodeResult } from "../wasm/nushell_wasm";
-
-export type Message = (
-  | { type: "user"; result: string }
-  | { type: "nushell"; result: RunCodeResult }
-) & { time: Date };
